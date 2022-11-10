99ce1dcbe9543582a8016f158a02930b5695b4bb
Manali Joshi <manalijoshi82@gmail.com>
2022-11-10 13:42:21 -0800

    updated with increment code

--- nov8.js
+++ nov8.js
@@ -11,6 +11,8 @@ function getNextChar(str) {
 
 console.log(getNextChar("manali")); // nbobmj
 
+
+
 /*
 function getNextChar(str) {
     let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
